9bef64569437e2e75f7fa769d02e7f0696ad146e
MagyarBene <magyar.bene@gmail.com>
2024-04-19 09:22:57 +0200

    m

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 import { apiKey } from "./apiKey.js";
 import { getData } from "./getData.js";
 
-const url = `https://api.api-ninjas.com/v1/airlines?name=${apiKey}`
+let options = {
+    method : 'GET',
+    headers : {'x-api-key' : '5by5k6zv55vIEVCi0pyjWrqodlmXP90l2MA0n1tg'}
+}
+const url = `https://api.api-ninjas.com/v1/airlines?name=Ryanair`
+
+fetch(url, options)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(`error ${err}`);
+    })
 
 let airline = []
 let page = 1
 let totalPage = 1
 let AlPerPage = 8
 
+document.querySelector('.pagination').addEventListener('click',handlePaginationClick)
+document.querySelector('.myBtn').addEventListener('click', handleClick)
 getData(url,renderAirlines)
+
+function handleClick(){
+    let AlName = document.querySelector('.myInput').value
+    //let url = AlName.length==0? returnUrl(apiKey) : returnUrl(apiKey,AlName)
+    getData(url+"Singapore Airlines",renderAirlines)
+    
+}
 
 function renderAirlines(data)
 {
@@ -22,11 +44,11 @@ function showAirline() {
     document.querySelector('.airline-list').innerHTML=""
     let startIndex=(page-1)*AlPerPage
     let endIndex=startIndex+AlPerPage
-    let AirlineToShow=cats.slice(startIndex,endIndex)
+    let AirlineToShow=airline.slice(startIndex,endIndex)
     console.log(AirlineToShow);
     AirlineToShow.forEach(obj=>{
         document.querySelector('.airline-list').innerHTML+=`
-        <div class="myCard"><img class="myPhoto" src="${obj.url}" alt="${obj.name[0].name}"></div>
+        <div class="myCard"><img class="myPhoto" src="${obj.logo_url}" alt="f"></div>
         `
     })
     renderPagination(airline.length)
@@ -50,3 +72,4 @@ function handlePaginationClick(e) {
         showAirline()
     }
  }
+ //getData(returnUrl(apiKey), renderAirlines)
