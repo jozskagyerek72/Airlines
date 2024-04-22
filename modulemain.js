67bc3b96e953f09576b36abeef6fe293ed2d5879
jozskagyerek72 <gillichbalint@gmail.com>
2024-04-22 19:16:56 +0200

    benivel nagy updateok

--- modulemain.js
+++ modulemain.js
@@ -1,7 +1,8 @@
 let airline = []
 
 let page = 1
-let qStr = "&query=Ryanair"
+let query = "&query="
+let qStr = "Ryanair"
 
 let options = {
     method : 'GET',
@@ -10,6 +11,24 @@ let options = {
 const url = `https://api.api-ninjas.com/v1/airlines?name=Ryanair`
 const urlpic = `https://api.unsplash.com/search/photos?client_id=Ajtp2bCaCXnMcwLDiOGZWtHh1vuCDM6bszAihbOe6g0&page=`
 //document.querySelector(".myBtn").addEventListener("click", renderAirlines)
+
+
+document.querySelector('#prev').addEventListener("click", prevPage)
+document.querySelector('#next').addEventListener("click", nextPage)
+
+function prevPage()
+{
+    document.querySelector(".airline-list").innerHTML=""
+    page=page-2
+    getData(urlpic+page+query+qStr, renderAirlines)
+}
+
+function nextPage()
+{
+    document.querySelector(".airline-list").innerHTML=""
+    getData(urlpic+page+query+qStr, renderAirlines)
+}
+
 
 fetch(url, options)
     .then(res => res.json())
@@ -35,10 +54,10 @@ function showAirlines(data)
 {
     airline.forEach(item => {
         document.querySelector('.airline-list').innerHTML+=`
-        <div class=" flex"><img class="" src="${item.logo_url}" alt="f"></div>
+        <div class=" card"><img class="img-fluid" src="${item.logo_url}" alt="f"></div>
         `
     })
-    getData(urlpic+page+qStr, renderAirlines)
+    getData(urlpic+page+query+qStr, renderAirlines)
 
 }
 
@@ -49,7 +68,14 @@ function renderAirlines(data){
         imageElement.alt = obj.alt_description
         document.querySelector(".airline-list").appendChild(imageElement)
     });
+
+    
+
     console.log(page);
     page++
+    
 }
+
+
+
 
